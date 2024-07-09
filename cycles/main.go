492ad658b8933c7e3d3cd5512a350720397182d0
macamilapz <macamilapz@gmail.com>
2024-07-08 22:46:13 -0500

    iterar numeros hasta donde determine el usuario

--- cycles/main.go
+++ cycles/main.go
@@ -1,16 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println(1)
-	fmt.Println(2)
-	fmt.Println(3)
-	fmt.Println(4)
-	fmt.Println(5)
-	fmt.Println(6)
-	fmt.Println(7)
-	fmt.Println(8)
-	fmt.Println(9)
-	fmt.Println(10)
+	// Declaramos una variable de tipo *bufio.Reader
+	var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+	// Indicamos al usuario que debe hacer
+	fmt.Printf("¿Hasta donde quiere contar sumerce?: ")
+	// Leemos lo que el usuario escriba hasta que le de enter
+	in, _ := reader.ReadString('\n')
+	// quitamos los dos ultimos caracteres para quede sin el "\n"
+	in = in[:len(in)-2]
+
+	// convertimos el input a numero
+	count, err := strconv.Atoi(in)
+	if err != nil {
+		panic("debes escribir un numero entero positivo")
+	}
+
+	for i := 1; i <= count; i++ {
+		fmt.Println(i)
+	}
+
 }
